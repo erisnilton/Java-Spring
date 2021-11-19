c904c52e458bb4fcf029a0b8002fe7985b7c211f
Erisnilton Freitas <erisniltongf@gmail.com>
2021-11-19 16:14:08 -0300

    Add files

---
+++ controle-de-acesso/src/main/java/com/erisnilton/controle/ControleApplication.java
@@ -0,0 +1,13 @@
+package com.erisnilton.controle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleApplication.class, args);
+	}
+
+}
