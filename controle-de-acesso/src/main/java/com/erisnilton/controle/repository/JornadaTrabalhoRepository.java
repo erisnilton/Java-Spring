863b2c44ca634a0420f519b949d1f3b32a915c50
Erisnilton Freitas <erisniltongf@gmail.com>
2021-11-25 15:47:47 -0300

    add repository of Jornada trabalho

---
+++ controle-de-acesso/src/main/java/com/erisnilton/controle/repository/JornadaTrabalhoRepository.java
@@ -0,0 +1,9 @@
+package com.erisnilton.controle.repository;
+
+import com.erisnilton.controle.model.JornadaTrabalho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JornadaTrabalhoRepository extends JpaRepository<JornadaTrabalho, Long> {
+}
