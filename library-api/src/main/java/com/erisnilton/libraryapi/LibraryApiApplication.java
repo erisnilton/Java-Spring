05e5c01d23d97f2dc996fc73da787eddf5ee595f
Erisnilton Freitas <erisniltongf@gmail.com>
2021-11-29 17:59:33 -0300

    Initial project library-api

---
+++ library-api/src/main/java/com/erisnilton/libraryapi/LibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.erisnilton.libraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApiApplication.class, args);
+	}
+
+}
