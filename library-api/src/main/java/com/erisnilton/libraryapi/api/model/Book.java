6369c4c0a894f300cf1ffd5d56d05631ca5eea42
Erisnilton Freitas <erisniltongf@gmail.com>
2021-11-30 11:21:04 -0300

    Add mapper of entity

--- library-api/src/main/java/com/erisnilton/libraryapi/api/model/Book.java
+++ library-api/src/main/java/com/erisnilton/libraryapi/api/model/Book.java
@@ -5,13 +5,26 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Book {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String title;
+
+    @Column
     private String author;
-    private String isbn;
+
+    @Column
+    private String ISBN;
 }
