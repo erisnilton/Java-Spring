039225bf8971ffbe109adcd8145179b14e0a8b43
Erisnilton Freitas <erisniltongf@gmail.com>
2021-11-30 11:24:18 -0300

    create hadleExceptions

--- library-api/src/main/java/com/erisnilton/libraryapi/api/resource/BookController.java
+++ library-api/src/main/java/com/erisnilton/libraryapi/api/resource/BookController.java
@@ -1,11 +1,17 @@
 package com.erisnilton.libraryapi.api.resource;
 
 import com.erisnilton.libraryapi.api.dto.BookDTO;
+import com.erisnilton.libraryapi.api.exception.ApiErrors;
 import com.erisnilton.libraryapi.api.model.Book;
 import com.erisnilton.libraryapi.api.service.BookService;
+import com.erisnilton.libraryapi.exception.BussinessException;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController()
 @RequestMapping("/api/books")
@@ -21,9 +27,23 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDTO create(@RequestBody BookDTO dto) {
+    public BookDTO create(@RequestBody @Valid BookDTO dto) {
         Book entity = modelMapper.map(dto, Book.class);
         entity = bookService.save(entity);
         return modelMapper.map(entity, BookDTO.class);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleValidationExceptions(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        return new ApiErrors(bindingResult);
+
+    }
+
+    @ExceptionHandler(BussinessException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleBusinessException( BussinessException ex) {
+        return  new ApiErrors(ex);
     }
 }
