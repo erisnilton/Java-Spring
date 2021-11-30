3b4fad18712cb43f51bed448cbe2bd16e0ac8484
Erisnilton Freitas <erisniltongf@gmail.com>
2021-11-30 11:22:56 -0300

    Implements tests that throw errors

--- library-api/src/test/java/com/erisnilton/libraryapi/api/resource/BookControllerTest.java
+++ library-api/src/test/java/com/erisnilton/libraryapi/api/resource/BookControllerTest.java
@@ -3,7 +3,10 @@ package com.erisnilton.libraryapi.api.resource;
 import com.erisnilton.libraryapi.api.dto.BookDTO;
 import com.erisnilton.libraryapi.api.model.Book;
 import com.erisnilton.libraryapi.api.service.BookService;
+import com.erisnilton.libraryapi.exception.BussinessException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,6 +23,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -40,17 +44,13 @@ public class BookControllerTest {
     @Test
     @DisplayName("Should be created book with success")
     public void createBookTest() throws Exception {
-        BookDTO dto = BookDTO.builder()
-                .title("my book")
-                .author("author")
-                .isbn("123456")
-                .build();
+        BookDTO dto = createNewBook();
 
         Book bookSave = Book.builder()
                 .id(001L)
                 .title("my book")
                 .author("author")
-                .isbn("123456")
+                .ISBN("123456")
                 .build();
 
         String json = new ObjectMapper().writeValueAsString(dto);
@@ -75,7 +75,52 @@ public class BookControllerTest {
 
     }
 
+
     @Test
     @DisplayName("Should throw error of validation when there is existing data insufficient for create book")
-    public void createIvalidBookTest() {}
+    public void createIvalidBookTest() throws Exception {
+        String json = new ObjectMapper().writeValueAsString(new BookDTO());
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(BOOK_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc.perform(request)
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("errors", hasSize(3)));
+
+    }
+
+    @Test
+    @DisplayName("Should throw erros if try create book with dupliacated isbn")
+    public void createBookWithDuplicationIsbn() throws Exception {
+        BookDTO dto = createNewBook();
+        String json = new ObjectMapper().writeValueAsString(dto);
+        String errorMessage = "Isbn already registered.";
+
+        BDDMockito.given(bookService.save(Mockito.any(Book.class)))
+                .willThrow(new BussinessException(errorMessage));
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(BOOK_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc.perform(request)
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("errors", hasSize(1)))
+                .andExpect(jsonPath("errors[0]").value(errorMessage));
+
+    }
+
+    private BookDTO createNewBook() {
+        return BookDTO.builder()
+                .title("my book")
+                .author("author")
+                .isbn("123456")
+                .build();
+    }
 }
