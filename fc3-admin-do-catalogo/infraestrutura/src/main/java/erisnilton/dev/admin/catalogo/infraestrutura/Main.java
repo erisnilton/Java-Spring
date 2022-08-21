30acd80877273c548dbe947115c5ae4b24a0b70e
Erisnilton Freitas <erisniltongf@gmail.com>
2022-08-21 17:16:43 -0300

    feat: finalizando a camada de infraestrutura

--- fc3-admin-do-catalogo/infraestrutura/src/main/java/erisnilton/dev/admin/catalogo/infraestrutura/Main.java
+++ fc3-admin-do-catalogo/infraestrutura/src/main/java/erisnilton/dev/admin/catalogo/infraestrutura/Main.java
@@ -1,9 +1,15 @@
 package erisnilton.dev.admin.catalogo.infraestrutura;
 
-import erisnilton.dev.admin.catalogo.application.UseCase;
+import erisnilton.dev.admin.catalogo.infraestrutura.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "production");
+        SpringApplication.run(WebServerConfig.class ,args);
     }
 }
---
+++ fc3-admin-do-catalogo/infraestrutura/src/main/java/erisnilton/dev/admin/catalogo/infraestrutura/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package erisnilton.dev.admin.catalogo.infraestrutura.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("erisnilton.dev.admin.catalogo")
+public class WebServerConfig {
+}
