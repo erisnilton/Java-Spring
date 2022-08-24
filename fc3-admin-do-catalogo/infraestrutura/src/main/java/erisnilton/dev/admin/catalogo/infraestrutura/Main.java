4b7a81ea41ed1d8d51e2bb9e93e3e44db35d1cff
Erisnilton Freitas <erisniltongf@gmail.com>
2022-08-24 15:12:49 -0300

    fix: remove application runner code

--- fc3-admin-do-catalogo/infraestrutura/src/main/java/erisnilton/dev/admin/catalogo/infraestrutura/Main.java
+++ fc3-admin-do-catalogo/infraestrutura/src/main/java/erisnilton/dev/admin/catalogo/infraestrutura/Main.java
@@ -1,13 +1,8 @@
 package erisnilton.dev.admin.catalogo.infraestrutura;
 
-import erisnilton.dev.admin.catalogo.application.category.create.CreateCategoryUseCase;
 import erisnilton.dev.admin.catalogo.infraestrutura.configuration.WebServerConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
@@ -16,12 +11,5 @@ public class Main {
         System.out.println("Hello world!");
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class ,args);
-    }
-
-
-    @Bean
-    @DependsOnDatabaseInitialization
-    ApplicationRunner runner (@Autowired  CreateCategoryUseCase createCategoryUseCase) {
-        return args -> {};
     }
 }
