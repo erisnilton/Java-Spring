31a41da2dbf6e42297e951d746b0659c4b1c0fba
Erisnilton Freitas <erisniltongf@gmail.com>
2022-08-26 11:34:49 -0300

    feat: implemantion of glabal handle execption

--- fc3-admin-do-catalogo/infraestrutura/src/test/java/erisnilton/dev/admin/catalogo/infraestrutura/api/CategoryAPITest.java
+++ fc3-admin-do-catalogo/infraestrutura/src/test/java/erisnilton/dev/admin/catalogo/infraestrutura/api/CategoryAPITest.java
@@ -5,7 +5,11 @@ import erisnilton.dev.admin.catalogo.ControllerTest;
 import erisnilton.dev.admin.catalogo.application.category.create.CreateCategoryOutput;
 import erisnilton.dev.admin.catalogo.application.category.create.CreateCategoryUseCase;
 import erisnilton.dev.admin.catalogo.domain.category.CategoryID;
+import erisnilton.dev.admin.catalogo.domain.exceptions.DomainException;
+import erisnilton.dev.admin.catalogo.domain.validation.Error;
+import erisnilton.dev.admin.catalogo.domain.validation.handler.Notification;
 import erisnilton.dev.admin.catalogo.infraestrutura.category.models.CreateCategoryApiInput;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,15 +19,16 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Objects;
 
+import static io.vavr.API.Left;
 import static io.vavr.API.Right;
+import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ControllerTest(controllers = CategoryAPI.class)
 public class CategoryAPITest {
@@ -65,4 +70,67 @@ public class CategoryAPITest {
                 ));
     }
 
+    @Test
+    public void givenInvalidName_whenCallsCreateCategory_thenShouldReturnNotification() throws Exception {
+
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should be not null";
+
+        final var aInput = new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        when(createCategoryUseCase.execute(any())).thenReturn(Left(Notification.create(new Error(expectedErrorMessage))));
+
+        final var request = post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(aInput));
+
+        this.mvc.perform(request)
+                .andDo(print())
+                .andExpectAll(
+                        status().isUnprocessableEntity(),
+                        header().string("Location", nullValue()),
+                        header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE),
+                        jsonPath("$.errors", hasSize(1)),
+                        jsonPath("$.errors[0].message", equalTo(expectedErrorMessage))
+                );
+        Mockito.verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
+    }
+
+    @Test
+    public void givenInvalidCommand_whenCallsCreateCategory_thenShouldReturnDomainException() throws Exception {
+
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should be not null";
+
+        final var aInput = new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        when(createCategoryUseCase.execute(any())).thenThrow(DomainException.with(new Error(expectedErrorMessage)));
+
+        final var request = post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(aInput));
+
+        this.mvc.perform(request)
+                .andDo(print())
+                .andExpectAll(
+                        status().isUnprocessableEntity(),
+                        header().string("Location", nullValue()),
+                        header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE),
+                        jsonPath("$.errors", hasSize(1)),
+                        jsonPath("$.errors[0].message", equalTo(expectedErrorMessage))
+                );
+        Mockito.verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
+    }
 }
