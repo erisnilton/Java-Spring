fbaac5e4e69bd58bd799baed108d320e931de959
Erisnilton Freitas <erisniltongf@gmail.com>
2022-08-29 15:42:05 -0300

    feat: Categorylistresponse class deserialization tests

---
+++ fc3-admin-do-catalogo/infraestrutura/src/test/java/erisnilton/dev/admin/catalogo/infraestrutura/category/models/CategoryListResponseTest.java
@@ -0,0 +1,46 @@
+package erisnilton.dev.admin.catalogo.infraestrutura.category.models;
+
+import erisnilton.dev.admin.catalogo.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.time.Instant;
+
+@JacksonTest
+public class CategoryListResponseTest {
+
+    @Autowired
+    private JacksonTester<CategoryListResponse> json;
+
+    @Test
+    public void testMarshall() throws Exception {
+
+        final var expectedId = "123";
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = false;
+        final var expectedCreatedAt = Instant.now();
+        final var expectedDeletedAt = Instant.now();
+
+        final var response = new CategoryListResponse(
+                expectedId,
+                expectedName,
+                expectedDescription,
+                expectedIsActive,
+                expectedCreatedAt,
+                expectedDeletedAt
+        );
+
+        final var actualJson = json.write(response);
+
+        Assertions.assertThat(actualJson)
+                .hasJsonPathValue("$.id", expectedId)
+                .hasJsonPathValue("$.name", expectedName)
+                .hasJsonPathValue("$.description", expectedDescription)
+                .hasJsonPathValue("$.created_at", expectedCreatedAt)
+                .hasJsonPathValue("$.deleted_at", expectedDeletedAt);
+
+    }
+}
