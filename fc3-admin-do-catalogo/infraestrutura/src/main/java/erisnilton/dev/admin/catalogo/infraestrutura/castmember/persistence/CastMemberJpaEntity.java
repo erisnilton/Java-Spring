621c45a8205feb6223b73ee107b9949798707f48
Erisnilton Freitas <erisniltongf@gmail.com>
2022-10-16 19:40:00 -0300

    feat: migrate castmember

--- fc3-admin-do-catalogo/infraestrutura/src/main/java/erisnilton/dev/admin/catalogo/infraestrutura/castmember/persistence/CastMemberJpaEntity.java
+++ fc3-admin-do-catalogo/infraestrutura/src/main/java/erisnilton/dev/admin/catalogo/infraestrutura/castmember/persistence/CastMemberJpaEntity.java
@@ -1,2 +1,104 @@
-package erisnilton.dev.admin.catalogo.infraestrutura.castmember.persistence;public class CastMemberJpaEntity {
+package erisnilton.dev.admin.catalogo.infraestrutura.castmember.persistence;
+
+import erisnilton.dev.admin.catalogo.domain.castmember.CastMember;
+import erisnilton.dev.admin.catalogo.domain.castmember.CastMemberID;
+import erisnilton.dev.admin.catalogo.domain.castmember.CastMemberType;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity(name = "CastMember")
+@Table(name = "cast_members")
+public class CastMemberJpaEntity {
+
+    @Id
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private CastMemberType type;
+
+    @Column(name = "created_at", nullable = false, columnDefinition = "DATETIME(6)")
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME(6)")
+    private Instant updatedAt;
+
+    public CastMemberJpaEntity() {
+    }
+
+    private CastMemberJpaEntity(
+            final String id,
+            final String name,
+            final CastMemberType type,
+            final Instant createdAt,
+            final Instant updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public static CastMemberJpaEntity from (CastMember aCastMember) {
+        return  new CastMemberJpaEntity(
+                aCastMember.getId().getValue(),
+                aCastMember.getName(),
+                aCastMember.getType(),
+                aCastMember.getCreatedAt(),
+                aCastMember.getUpdatedAt()
+        );
+    }
+
+    public CastMember toAgregate() {
+        return CastMember.with(
+                CastMemberID.from(getId()),
+                getName(),
+                getType(),
+                getCreatedAt(),
+                getUpdatedAt()
+        );
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CastMemberType getType() {
+        return type;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setId(final String id) {
+        this.id = id;
+    }
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public void setType(final CastMemberType type) {
+        this.type = type;
+    }
+
+    public void setCreatedAt(final Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(final Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
--- fc3-admin-do-catalogo/infraestrutura/src/main/java/erisnilton/dev/admin/catalogo/infraestrutura/castmember/persistence/CastMemberRepository.java
+++ fc3-admin-do-catalogo/infraestrutura/src/main/java/erisnilton/dev/admin/catalogo/infraestrutura/castmember/persistence/CastMemberRepository.java
@@ -1,4 +1,12 @@
 package erisnilton.dev.admin.catalogo.infraestrutura.castmember.persistence;
 
-public interface CastMemberRepository {
+import erisnilton.dev.admin.catalogo.infraestrutura.category.persistence.CategoryJpaEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CastMemberRepository extends JpaRepository<CategoryJpaEntity, String> {
+
+    Page<CategoryJpaEntity> findAll(Specification<CategoryJpaEntity> whereClause, Pageable page);
 }
